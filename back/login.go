8544c545b598ee354862a88d12ba3632f383703d
ctake <ctake0099@gmail.com>
2024-10-17 20:43:45 +0900

    create get profile

--- back/login.go
+++ back/login.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -12,20 +14,16 @@ import (
 
 // Spotify APIのOAuth2設定
 var spotifyConfig = &oauth2.Config{
-	
-	ClientID:"",// Spotify Developerから取得
-	ClientSecret: "",    // Spotify Developerから取得
-
-
-	RedirectURL:  "http://localhost:8080/", // リダイレクトURL
-	Endpoint:     spotify.Endpoint,        // Spotify用のOAuth2エンドポイント
+	ClientID:     "", // Spotify Developerから取得
+	ClientSecret: "", // Spotify Developerから取得
+	RedirectURL:  "http://localhost:8080/",           // リダイレクトURL
+	Endpoint:     spotify.Endpoint,                   // Spotify用のOAuth2エンドポイント
 	Scopes: []string{
 		"user-read-private", "user-read-email", // 必要なスコープを指定
 	},
 }
 
 var oauthStateString = "random"
-
 
 // メイン関数でサーバーを開始
 func main() {
@@ -45,27 +43,44 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 // /callbackエンドポイント: 認証後に呼び出される
 func handleCallback(w http.ResponseWriter, r *http.Request) {
 	state := r.FormValue("state")
-	fmt.Println("aaaa")
 	if state != oauthStateString {
-		fmt.Println(state)
-		fmt.Println(oauthStateString)
-
 		log.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
-		fmt.Println("aaa")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
 	code := r.FormValue("code")
 	token, err := spotifyConfig.Exchange(context.Background(), code)
-	fmt.Println("token",token)
 	if err != nil {
 		log.Printf("Code exchange failed with '%s'\n", err)
-		http.Redirect(w, r, "http://localhost:8080/", http.StatusTemporaryRedirect)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
-	fmt.Fprintf(w, "Access Token: %s\n", token.AccessToken)
+	// 取得したアクセストークンでSpotify APIを呼び出してユーザープロフィールを取得
+	client := spotifyConfig.Client(context.Background(), token)
+	resp, err := client.Get("https://api.spotify.com/v1/me")
+	if err != nil {
+		log.Printf("Failed to get user profile: %s\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Failed to read response body: %s\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	// ユーザープロフィール情報をJSONとして解析して表示
+	var userProfile map[string]interface{}
+	if err := json.Unmarshal(body, &userProfile); err != nil {
+		log.Printf("Failed to unmarshal user profile: %s\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprintf(w, "User Profile: %v\n", userProfile)
 }
-
-

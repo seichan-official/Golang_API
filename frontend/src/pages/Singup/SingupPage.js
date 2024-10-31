09fa44952e29c9dd775e6d531caebb5d1968cb69
Seiya <seichan_official@SeiyanoMacBook-Air.local>
2024-10-31 20:55:35 +0900

    add

--- frontend/src/pages/Singup/SingupPage.js
+++ frontend/src/pages/Singup/SingupPage.js
@@ -13,51 +13,12 @@ const SignupPage = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // ここでサインアップの処理を行います
-    console.log('ユーザー登録:', formData);
-  };
-
   return (
     <div className="signup-page">
-      <h2>サインアップ</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">ユーザー名</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">メールアドレス</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">パスワード</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="signup-button">登録する</button>
-      </form>
+      <h2>サインアップは現在開発中となります。 </h2>
+      <h2>
+          開発完了するまではSpotifyアカウントでお楽しみください。
+      </h2>
     </div>
   );
 };
